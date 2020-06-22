2845eae347915357a3e24c18fd44cc192475f2ed
Julian <jcastro21@belenwolverines.org>
2020-06-22 10:31:36 -0400

    Beginning to add users

--- app/controllers/helpers/application_helper.rb
+++ app/controllers/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    def gravatar_for(user, options = { size: 80})
+        hash = Digest::MD5.hexdigest(user.email.downcase)
+        gravatar_url = "https://www.gravatar.com/avatar/#{hash}"
+        image_tag(gravatar_url, alt: user.username)
+    end
+
+
+    def wee?
+
+        "hello!!"
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+    
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
+    
     def new
         @user = User.new
     end
@@ -26,6 +32,10 @@ class UsersController < ApplicationController
         else
             render 'edit'
         end
+    end
+
+    def index
+        @users = User.all
     end
 
     private
